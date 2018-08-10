2d484aa3eb41bbed4f3d88325b562af0e2a23cd6
mattpodolak <mpodola2@gmail.com>
2018-08-10 16:07:41 -0400

    added mentor db

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from app import db
 
-class User(db.Model):
+class Mentor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,17 +10,18 @@ class User(db.Model):
     #posts is not a db field, defined one the "one" side of one-to-many relation
     #backref defines name of field for the "many" objs
     #lazy defines database query for relationship
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    #in db.Fore... reference user.id user is database table name, referencing the id from this table
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    users = db.relationship('User', backref='mentor', lazy='dynamic')
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    #in db.Fore... reference user.id user is database table name, referencing the id from this table
+    mentor_id = db.Column(db.Integer, db.ForeignKey('mentor.id'))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
--- mentorship.py
+++ mentorship.py
@@ -1,7 +1,7 @@
 from app import flapp, db
-from app.models import User, Post
+from app.models import User, Mentor
 
 #flask shell invokes this function, registering included items
 @flapp.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Mentor': Mentor}
