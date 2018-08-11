88fdccc265aa56bb703ac275c5a920f0ff83153e
mattpodolak <mpodola2@gmail.com>
2018-08-11 17:00:00 -0400

    Added more pages

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,19 @@
 from app import flapp
 from app.forms import LoginForm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Mentor
+from werkzeug.urls import url_parse
 
 @flapp.route('/')
 @flapp.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home')
+
+@flapp.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', title='Dashboard')
 
 @flapp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -31,7 +26,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+                next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @flapp.route('/logout')
