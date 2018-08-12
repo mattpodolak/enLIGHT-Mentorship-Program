6a4c99582282821845146ca79c1e5d99c3bcc8a5
mattpodolak <mpodola2@gmail.com>
2018-08-12 13:50:11 -0400

    troubleshooting for serving profile

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from app import flapp
+from app import flapp, db
 from app.forms import LoginForm, RegistrationForm
-from flask import render_template, flash, redirect, request
+from flask import render_template, flash, redirect, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Mentor
 from werkzeug.urls import url_parse
+import sys
 
 @flapp.route('/')
 @flapp.route('/index')
@@ -55,6 +56,22 @@ def logout():
 @flapp.route('/user/<id>')
 @login_required
 def user(id):
-    user = User.query.filter_by(id=id).first_or_404()
-    print(user)
-    return render_template('user.html', user=user)
+    user = User.query.filter_by(id=id)
+    try:
+        # check if user is defined
+        user.id #maybe this should be user.id
+    except AttributeError:
+        print('USER NOT DEFINED', file=sys.stderr)
+        # check if mentor is defined
+        mentor = Mentor.query.filter_by(id=id)
+        try:
+            mentor.id #maybe this should be mentor.id
+        except AttributeError:
+            # return 404 if not mentor or user
+            return render_template('404.html')
+        else:
+            is_mentor = True
+            return render_template('user.html', user=mentor, is_mentor=is_mentor)
+    else:
+        is_mentor = False
+        return render_template('user.html', user=user, is_mentor=is_mentor)
