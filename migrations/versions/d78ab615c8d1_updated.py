370aa981bd116c6d41eb6a52fe1cae437a2aa02b
mattpodolak <mpodola2@gmail.com>
2018-08-15 05:00:53 -0400

    db stuff

---
+++ migrations/versions/d78ab615c8d1_updated.py
@@ -0,0 +1,24 @@
+"""updated
+
+Revision ID: d78ab615c8d1
+Revises: 
+Create Date: 2018-08-15 05:00:28.841957
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd78ab615c8d1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
